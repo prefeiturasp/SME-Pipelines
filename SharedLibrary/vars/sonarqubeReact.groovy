4da25d23930461e7f75f87b910be7eb8b1418907
Bruna Lima <thamirys.lima@spassu.com.br>
2025-11-28 19:34:48 -0300

    teste biblioteca

---
+++ SharedLibrary/vars/sonarqubeReact.groovy
@@ -0,0 +1,41 @@
+#!/usr/bin/env groovy
+
+def call(Map stageParams) {
+
+    def nodeVersion = stageParams.nodeVersion
+    def scannerHome = tool 'sonarscanner-7.2.0';
+    def coverageExclusions = stageParams.coverageExclusions
+    def sonarExclusions = stageParams.sonarExclusions
+    def dockerfilePath = stageParams.dockerfilePath
+
+    nodejs(cacheLocationStrategy: workspace(), nodeJSInstallationName: nodeVersion) {
+        withSonarQubeEnv('sonarqube-sme'){
+            if (!env.BRANCH_NAME.startsWith('PR-')) {
+                sh"""
+                    ${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=${SONAR_PROJECT} \
+                        -Dsonar.branch.name=${branchname} \
+                        -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
+                        -Dsonar.exclusions="${coverageExclusions}" \
+                        -Dsonar.coverage.exclusions="${coverageExclusions}"  \
+                        -Dsonar.docker.file.patterns=${dockerfilePath} \
+                        -Dsonar.sources=.
+                """
+            } else {
+                sh"""
+                    ${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=${SONAR_PROJECT} \
+                        -Dsonar.pullrequest.branch=${env.CHANGE_BRANCH} \
+                        -Dsonar.pullrequest.base=${env.CHANGE_TARGET} \
+                        -Dsonar.pullrequest.key=${env.CHANGE_ID} \
+                        -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
+                        -Dsonar.typescript.tsconfigPath=tsconfig.json \
+                        -Dsonar.exclusions="${coverageExclusions}" \
+                        -Dsonar.coverage.exclusions="${coverageExclusions}"  \
+                        -Dsonar.docker.file.patterns=${dockerfilePath} \
+                        -Dsonar.sources=.
+                """
+            }
+        }
+    }
+}
