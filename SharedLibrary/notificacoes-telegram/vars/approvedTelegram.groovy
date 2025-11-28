50bbb30a56f069913c9d3a8396a81a57301790b6
Bruna Lima <thamirys.lima@spassu.com.br>
2025-11-28 11:50:55 -0300

    teste biblioteca

--- SharedLibrary/notificacoes-telegram/vars/approvedTelegram.groovy
+++ SharedLibrary/notificacoes-telegram/vars/approvedTelegram.groovy
@@ -3,7 +3,7 @@
 def call(String chatIdCredential, String approvedBy) {
 
     def messageTemplate = (
-        "<b>REALIZADA APROVAÇÃO PARA O JOB:</b> <a href='${env.JOB_URL}'>${env.JOB_NAME}</a>\n\n" +
+        "<b>✅ REALIZADA APROVAÇÃO PARA O JOB:</b> <a href='${env.JOB_URL}'>${env.JOB_NAME}</a>\n\n" +
         "<b>Build Number:</b> <a href='${env.BUILD_URL}'>${env.BUILD_DISPLAY_NAME}</a>" +
         "<b>Aprovado por:</b> ${approvedBy}"
     )
