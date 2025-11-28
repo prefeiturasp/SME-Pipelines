396d5e06e5d1677031aa0ecf5181630ab79b1f3f
Bruna Lima <thamirys.lima@spassu.com.br>
2025-11-28 19:41:17 -0300

    teste biblioteca

---
+++ SharedLibrary/vars/sonarqubePython.groovy
@@ -0,0 +1,37 @@
+#!/usr/bin/env groovy
+
+def call(Map stageParams) {
+
+    def scannerHome = tool 'sonarscanner-7.2.0';
+    def coverageExclusions = stageParams.coverageExclusions
+    def sonarExclusions = stageParams.sonarExclusions
+    def dockerfilePath = stageParams.dockerfilePath
+
+    withSonarQubeEnv('sonarqube-sme'){
+        if (!env.BRANCH_NAME.startsWith('PR-')) {
+            sh"""
+                ${scannerHome}/bin/sonar-scanner \
+                    -Dsonar.projectKey=${SONAR_PROJECT} \
+                    -Dsonar.branch.name=${branchname} \
+                    -Dsonar.python.coverage.reportPaths=coverage.xml \
+                    -Dsonar.exclusions="${coverageExclusions}" \
+                    -Dsonar.coverage.exclusions="${coverageExclusions}"  \
+                    -Dsonar.docker.file.patterns=${dockerfilePath} \
+                    -Dsonar.sources=.
+            """
+        } else {
+            sh"""
+                ${scannerHome}/bin/sonar-scanner \
+                    -Dsonar.projectKey=${SONAR_PROJECT} \
+                    -Dsonar.pullrequest.branch=${env.CHANGE_BRANCH} \
+                    -Dsonar.pullrequest.base=${env.CHANGE_TARGET} \
+                    -Dsonar.pullrequest.key=${env.CHANGE_ID} \
+                    -Dsonar.python.coverage.reportPaths=coverage.xml \
+                    -Dsonar.exclusions="${coverageExclusions}" \
+                    -Dsonar.coverage.exclusions="${coverageExclusions}"  \
+                    -Dsonar.docker.file.patterns=${dockerfilePath} \
+                    -Dsonar.sources=.
+            """
+        }
+    }
+}
