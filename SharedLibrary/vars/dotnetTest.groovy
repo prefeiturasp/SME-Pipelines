d3e2bc8fbd8f2dae44a994a389f04d4ed694b24f
Bruna Lima <thamirys.lima@spassu.com.br>
2025-12-09 19:04:38 -0300

    bibliotecas

--- SharedLibrary/vars/dotnetTest.groovy
+++ SharedLibrary/vars/dotnetTest.groovy
@@ -19,6 +19,13 @@ def call(Map tests) {
                                 dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
                             """
                             stash includes: "${config.projectPath}/coverage.xml", name: config.stashName, allowEmpty: true
+                        } else if (config.testTool == "dotnet test") {
+                            sh """
+                                dotnet tool install --global coverlet.console
+                                export PATH="\$PATH:/home/jenkins/.dotnet/tools"
+                                dotnet test ${config.projectPath} /p:CollectCoverage=true /p:CoverletOutputFormat=opencover --collect 'XPlat Code Coverage'
+                            """
+                            stash includes: "${config.projectPath}/coverage.opencover.xml", name: config.stashName, allowEmpty: true
                         } else {
                             echo "Tool não definida."
                         }
