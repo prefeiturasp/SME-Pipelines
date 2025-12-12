6260e7a05daa73f6af411846edabdf8e28c8ecb2
Bruna Lima <thamirys.lima@spassu.com.br>
2025-12-12 16:53:08 -0300

    bibliotecas

--- SharedLibrary/vars/testDotnet.groovy
+++ SharedLibrary/vars/testDotnet.groovy
@@ -1,54 +1,32 @@
 #!/usr/bin/env groovy
 
-def call(Map tests) {
+def call(Map config) {
 
-    def tasks = [:]
+    withDotNet(sdk: config.dotnetVersion) {
+        retry(2) {
+            if (config.testTool == "dotnet-test") {
+                dotnetTest(
+                    project: config.projectPath,
+                    properties: [
+                        CollectCoverage: 'true',
+                        CoverletOutputFormat: 'opencover'
+                    ],
+                    collect: 'Code Coverage',
+                    noBuild: false,
+                    continueOnError: false
+                )
+                stash includes: "${config.projectPath}/coverage.opencover.xml", name: config.stashName, allowEmpty: true
+            }
 
-    tests.each { testName, config ->
-
-        tasks[testName] = {
-            stage(testName) {
-                agent {
-                    kubernetes {
-                        label 'builder-debian'
-                        defaultContainer 'builder-debian'
-                    }
-                }
-
-                steps {
-                    script {
-                        withDotNet(sdk: config.dotnetVersion) {
-                            retry(2) {
-                                if (config.testTool == "dotnet-test") {
-                                    dotnetTest(
-                                        project: config.projectPath,
-                                        properties: [
-                                            CollectCoverage: 'true',
-                                            CoverletOutputFormat: 'opencover'
-                                        ],
-                                        collect: 'Code Coverage',
-                                        noBuild: false,
-                                        continueOnError: false
-                                    )
-                                    stash includes: "${config.projectPath}/coverage.opencover.xml", name: config.stashName, allowEmpty: true
-                                }
-
-                                if (config.testTool == "dotnet-coverage") {
-                                    sh """
-                                        dotnet tool install --global dotnet-coverage
-                                        export PATH="\$PATH:/home/jenkins/.dotnet/tools"
-                                        cd ${config.projectPath}
-                                        dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
-                                    """
-                                    stash includes: "${config.projectPath}/coverage.xml", name: config.stashName, allowEmpty: true
-                                }
-                            }
-                        }
-                    }
-                }
+            if (config.testTool == "dotnet-coverage") {
+                sh """
+                    dotnet tool install --global dotnet-coverage
+                    export PATH="\$PATH:/home/jenkins/.dotnet/tools"
+                    cd ${config.projectPath}
+                    dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
+                """
+                stash includes: "${config.projectPath}/coverage.xml", name: config.stashName, allowEmpty: true
             }
         }
     }
-
-    parallel tasks
 }
