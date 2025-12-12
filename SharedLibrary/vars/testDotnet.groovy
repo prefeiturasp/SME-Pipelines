a31497ca2bfd9443fc962cc02610c6942efe7d27
Bruna Lima <thamirys.lima@spassu.com.br>
2025-12-12 14:26:24 -0300

    bibliotecas

--- SharedLibrary/vars/testDotnet.groovy
+++ SharedLibrary/vars/testDotnet.groovy
@@ -7,35 +7,32 @@ def call(Map tests) {
     tests.each { testName, config ->
 
         tasks[testName] = {
-            stage(testName) {
-                agent { kubernetes { label 'builder-debian'; defaultContainer 'builder-debian' } }
-                withDotNet(sdk: config.dotnetVersion) {
-                    retry(2) {
-                        if (config.testTool == "dotnet-coverage") {
-                            sh """
-                                dotnet tool install --global dotnet-coverage
-                                export PATH="\$PATH:/home/jenkins/.dotnet/tools"
-                                cd ${config.projectPath}
-                                dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
-                            """
-                            stash includes: "${config.projectPath}/coverage.xml", name: "${config.stashName}", allowEmpty: true
-                        
-                        } else if (config.testTool == "dotnet-test") {
-                            dotnetTest(
-                                project: config.projectPath,
-                                properties: [
-                                    CollectCoverage: 'true',
-                                    CoverletOutputFormat: 'opencover'
-                                ],
-                                collect: 'Code Coverage',
-                                noBuild: false,
-                                continueOnError: false
-                            )
-                            stash includes: "${config.projectPath}/coverage.opencover.xml", name: "${config.stashName}", allowEmpty: true
-                        
-                        } else {
-                            echo "Tool não definida."
-                        }
+            withDotNet(sdk: config.dotnetVersion) {
+                retry(2) {
+                    if (config.testTool == "dotnet-coverage") {
+                        sh """
+                            dotnet tool install --global dotnet-coverage
+                            export PATH="\$PATH:/home/jenkins/.dotnet/tools"
+                            cd ${config.projectPath}
+                            dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
+                        """
+                        stash includes: "${config.projectPath}/coverage.xml", name: "${config.stashName}", allowEmpty: true
+                    
+                    } else if (config.testTool == "dotnet-test") {
+                        dotnetTest(
+                            project: config.projectPath,
+                            properties: [
+                                CollectCoverage: 'true',
+                                CoverletOutputFormat: 'opencover'
+                            ],
+                            collect: 'Code Coverage',
+                            noBuild: false,
+                            continueOnError: false
+                        )
+                        stash includes: "${config.projectPath}/coverage.opencover.xml", name: "${config.stashName}", allowEmpty: true
+                    
+                    } else {
+                        echo "Tool não definida."
                     }
                 }
             }
