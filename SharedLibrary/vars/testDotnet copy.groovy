719607823501424dba9348ca32e92077fe5a6a02
Bruna Lima <thamirys.lima@spassu.com.br>
2025-12-12 17:26:22 -0300

    bibliotecas

---
+++ SharedLibrary/vars/testDotnet copy.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/env groovy
+
+def call(Map config) {
+
+    withDotNet(sdk: config.dotnetVersion) {
+        env.failedStage = env.STAGE_NAME
+        retry(2) {
+            if (config.testTool == "dotnet-test") {
+                dotnetTest(
+                    project: config.projectPath,
+                    properties: [
+                        CollectCoverage: 'true',
+                        CoverletOutputFormat: 'opencover'
+                    ],
+                    collect: 'Code Coverage',
+                    noBuild: false,
+                    continueOnError: false
+                )
+                stash includes: "${config.projectPath}/coverage.opencover.xml", name: config.stashName, allowEmpty: true
+            }
+
+            if (config.testTool == "dotnet-coverage") {
+                sh """
+                    dotnet tool install --global dotnet-coverage
+                    export PATH="\$PATH:/home/jenkins/.dotnet/tools"
+                    cd ${config.projectPath}
+                    dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
+                """
+                stash includes: "${config.projectPath}/coverage.xml", name: config.stashName, allowEmpty: true
+            }
+        }
+    }
+}
