d99dc0ded9bacbc66e2572967b9a46fcd36d1993
Bruna Lima <thamirys.lima@spassu.com.br>
2025-12-22 09:20:03 -0300

    bibliotecas

--- SharedLibrary/vars/buildImage.groovy
+++ SharedLibrary/vars/buildImage.groovy
@@ -5,7 +5,7 @@ def call(Map stageParams) {
     def fullImageName = ""
     withCredentials([string(credentialsId: "${env.registryUrl}", variable: 'registryUrl')]) {
         fullImageName = "${registryUrl}/${env.BRANCH_NAME}/${stageParams.imageName}"
-        def stageParams.dockerImage = docker.build(fullImageName, "-f ${stageParams.dockerfilePath} .")
+        def dockerImage = docker.build(fullImageName, "-f ${stageParams.dockerfilePath} .")
 
         if (config.sendRegistry == "yes") {
             docker.withRegistry("https://${registryUrl}", env.registryCredential) {
