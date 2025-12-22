b78cbe210cd0373dbea7ed156a7a1292ffecdd48
Bruna Lima <thamirys.lima@spassu.com.br>
2025-12-22 09:58:17 -0300

    bibliotecas

--- SharedLibrary/vars/restartDeployment.groovy
+++ SharedLibrary/vars/restartDeployment.groovy
@@ -2,16 +2,13 @@
 
 def call(Map stageParams) {
 
-    restarts[stageParams.deploymentName] = {
+    sh """
+        [ -f "\$HOME/.kube/config" ] && rm -f "\$HOME/.kube/config"
 
-        sh """
-            [ -f "\$HOME/.kube/config" ] && rm -f "\$HOME/.kube/config"
+        mkdir -p "\$HOME/.kube"
+        cp "\$config" "\$HOME/.kube/config"
+        export KUBECONFIG="\$HOME/.kube/config"
 
-            mkdir -p "\$HOME/.kube"
-            cp "\$config" "\$HOME/.kube/config"
-            export KUBECONFIG="\$HOME/.kube/config"
-
-            kubectl rollout restart deployment/${stageParams.deploymentName} -n ${stageParams.namespace}
-        """
-    }
+        kubectl rollout restart deployment/${stageParams.deploymentName} -n ${stageParams.namespace}
+    """
 }
