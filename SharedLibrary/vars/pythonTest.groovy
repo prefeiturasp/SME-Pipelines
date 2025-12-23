42b5c08296f0df6fc5b26594f861d996439cc7b3
Bruna Lima <thamirys.lima@spassu.com.br>
2025-12-23 15:31:13 -0300

    bibliotecas

--- SharedLibrary/vars/pythonTest.groovy
+++ SharedLibrary/vars/pythonTest.groovy
@@ -9,21 +9,19 @@ def call(Map stageParams) {
 
     retry(2) {
         sh 'pip install --root-user-action=ignore --upgrade pip'
-        sh "pip install --root-user-action=ignore --no-cache-dir -r ${requirementsDir}"
+        sh 'pip install --root-user-action=ignore --no-cache-dir -r "${requirementsDir}"'
         sh 'pip install --root-user-action=ignore coverage'
         withCredentials([file(credentialsId: credentialEnv, variable: 'env')]) {
-            sh '''
-                touch .env
-                cp "$env" ".env"
-                export DJANGO_READ_DOT_ENV_FILE=True
-            '''
+            sh 'touch .env'
+            sh 'cp "$env" ".env'
+            sh 'export DJANGO_READ_DOT_ENV_FILE=True'
             
             if (withMigration == "true") {
-                sh "python manage.py migrate"
+                sh 'python manage.py migrate'
             }
 
-            sh "${testCommand}"
-            sh "coverage xml"
+            sh '"${testCommand}"'
+            sh 'coverage xml'
         }
         
         stash name: 'coverage', includes: 'coverage.xml'
