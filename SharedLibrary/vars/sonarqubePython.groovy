7ee4f97649dea0f433d9f8dfc5a4f0a0ba1e9e1c
Bruna Lima <thamirys.lima@spassu.com.br>
2026-01-12 14:09:18 -0300

    libs - update sonarqube python

--- SharedLibrary/vars/sonarqubePython.groovy
+++ SharedLibrary/vars/sonarqubePython.groovy
@@ -12,36 +12,32 @@ def call(Map stageParams) {
         unstash "coverage"
         
         if (!env.BRANCH_NAME.startsWith('PR-')) {
-            retry(1) {
-                sh"""
-                    ${scannerHome}/bin/sonar-scanner \
-                        -Dsonar.projectKey=${SONAR_PROJECT} \
-                        -Dsonar.branch.name=${branchname} \
-                        -Dsonar.python.coverage.reportPaths=coverage.xml \
-                        -Dsonar.exclusions="${coverageExclusions}" \
-                        -Dsonar.coverage.exclusions="${coverageExclusions}"  \
-                        -Dsonar.docker.file.patterns=${dockerfilePath} \
-                        -Dsonar.sources=.
-                """
-            }
+            sh"""
+                ${scannerHome}/bin/sonar-scanner \
+                    -Dsonar.projectKey=${SONAR_PROJECT} \
+                    -Dsonar.branch.name=${branchname} \
+                    -Dsonar.python.coverage.reportPaths=coverage.xml \
+                    -Dsonar.exclusions="${coverageExclusions}" \
+                    -Dsonar.coverage.exclusions="${coverageExclusions}"  \
+                    -Dsonar.docker.file.patterns=${dockerfilePath} \
+                    -Dsonar.sources=.
+            """
         } else {
-            retry(1) {
-                sh "git fetch origin refs/heads/${env.CHANGE_TARGET}:refs/remotes/origin/${env.CHANGE_TARGET}"
-                sh "git show-ref | grep ${env.CHANGE_TARGET}"
-                
-                sh"""
-                    ${scannerHome}/bin/sonar-scanner \
-                        -Dsonar.projectKey=${SONAR_PROJECT} \
-                        -Dsonar.pullrequest.branch=${env.CHANGE_BRANCH} \
-                        -Dsonar.pullrequest.base=${env.CHANGE_TARGET} \
-                        -Dsonar.pullrequest.key=${env.CHANGE_ID} \
-                        -Dsonar.python.coverage.reportPaths=coverage.xml \
-                        -Dsonar.exclusions="${coverageExclusions}" \
-                        -Dsonar.coverage.exclusions="${coverageExclusions}"  \
-                        -Dsonar.docker.file.patterns=${dockerfilePath} \
-                        -Dsonar.sources=.
-                """
-            }
+            sh "git fetch origin refs/heads/${env.CHANGE_TARGET}:refs/remotes/origin/${env.CHANGE_TARGET}"
+            sh "git show-ref | grep ${env.CHANGE_TARGET}"
+            
+            sh"""
+                ${scannerHome}/bin/sonar-scanner \
+                    -Dsonar.projectKey=${SONAR_PROJECT} \
+                    -Dsonar.pullrequest.branch=${env.CHANGE_BRANCH} \
+                    -Dsonar.pullrequest.base=${env.CHANGE_TARGET} \
+                    -Dsonar.pullrequest.key=${env.CHANGE_ID} \
+                    -Dsonar.python.coverage.reportPaths=coverage.xml \
+                    -Dsonar.exclusions="${coverageExclusions}" \
+                    -Dsonar.coverage.exclusions="${coverageExclusions}"  \
+                    -Dsonar.docker.file.patterns=${dockerfilePath} \
+                    -Dsonar.sources=.
+            """
         }
     }
 }
