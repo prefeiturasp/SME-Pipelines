634599b925cd2cac9fd2b022305533c6f9f65091
Bruna Lima <thamirys.lima@spassu.com.br>
2026-01-14 08:38:44 -0300

    libs -  para bloquear commits de QAs

--- SharedLibrary/vars/blockExecution.groovy
+++ SharedLibrary/vars/blockExecution.groovy
@@ -4,7 +4,7 @@ def call(Map stageParams) {
     def blockedAuthors = [
         'bot-ci',
         'dependabot[bot]',
-        'wilson-calixto'
+        'Wilson Calixto Cordovil junior'
     ]
 
     // PR ou branch
