e4efe5929804feaefbe0704737ca72d9abc22747
Bruna Lima <bruna.lima9254@gmail.com>
2026-01-14 13:38:22 -0300

    libs - para bloquear start com commits de QAs

--- SharedLibrary/vars/blockExecution.groovy
+++ SharedLibrary/vars/blockExecution.groovy
@@ -1,30 +1,29 @@
 def call(Map stageParams) {
 
-    def blockedAuthors = [
-        'bot-ci',
-        'dependabot[bot]',
-        'Wilson Calixto Cordovil junior'
-    ]
+    withCredentials([
+        string(credentialsId: 'BlockStartTimeQA', variable: 'blockedAuthors')
+    ]) {
 
-    def author = env.CHANGE_ID
-        ? sh(
-            script: "git log origin/${env.CHANGE_TARGET}..HEAD --pretty=format:%an | sort -u",
-            returnStdout: true
-            ).trim()
-        : sh(
-            script: "git log -1 --pretty=format:%an",
-            returnStdout: true
-            ).trim()
-    
-    echo "Author(es) do change:\n${author}"
+        def author = env.CHANGE_ID
+            ? sh(
+                script: "git log origin/${env.CHANGE_TARGET}..HEAD --pretty=format:%an | sort -u",
+                returnStdout: true
+                ).trim()
+            : sh(
+                script: "git log -1 --pretty=format:%an",
+                returnStdout: true
+                ).trim()
+        
+        echo "Author(es) do change:\n${author}"
 
-    def authors = author.split('\n')
-    def blocked = authors.findAll { it in blockedAuthors }
+        def authors = author.split('\n')
+        def blocked = authors.findAll { it in blockedAuthors }
 
-    if (blocked) {
-        currentBuild.result = 'ABORTED'
-        error("""
-            ⚠️ Pipeline cancelada pois o commit foi realizado pelo time de QA!
-        """)
+        if (blocked) {
+            currentBuild.result = 'ABORTED'
+            error("""
+                ⚠️ Pipeline cancelada pois o commit foi realizado pelo time de QA!
+            """)
+        }
     }
 }
