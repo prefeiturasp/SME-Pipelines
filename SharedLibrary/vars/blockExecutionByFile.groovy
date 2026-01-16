382872e273dec9654cd80da6b7fe2310f20bf3f3
Bruna Lima <bruna.lima9254@gmail.com>
2026-01-15 22:01:56 -0300

    add lib - blockExecutionByFile

---
+++ SharedLibrary/vars/blockExecutionByFile.groovy
@@ -0,0 +1,24 @@
+def call(Map stageParams) {
+
+    def listFiles = env.CHANGE_ID
+        ? sh(
+            script: "git diff --name-status ${env.CHANGE_TARGET}^ HEAD | cut -f2- > ./commit_files_all",
+            returnStdout: true
+            ).trim()
+        : sh(
+            script: "git diff --name-status HEAD^ HEAD | cut -f2- > ./commit_files_all",
+            returnStdout: true
+            ).trim()
+    
+    def listedFiles = listFiles.split('\n')
+    def blocked = listedFiles.findAll { it in blockedAuthors }
+
+    echo "Change:\n${listFiles}"
+
+    // if (blocked) {
+    //     currentBuild.result = 'ABORTED'
+    //     error("""
+    //         ⚠️ Pipeline cancelada pois o commit foi realizado pelo time de QA!
+    //     """)
+    // }
+}
