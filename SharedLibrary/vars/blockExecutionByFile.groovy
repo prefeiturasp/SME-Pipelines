b3e6e8cf352c5abb0b145c2786c3c0af8b133fe4
Bruna Lima <bruna.lima9254@gmail.com>
2026-01-16 09:26:01 -0300

    libs - para bloquear start com commits de QAs

--- SharedLibrary/vars/blockExecutionByFile.groovy
+++ SharedLibrary/vars/blockExecutionByFile.groovy
@@ -1,14 +1,9 @@
 def call(Map stageParams) {
 
-    def listFiles = env.CHANGE_ID
-        ? sh(
-            script: "git fetch origin ${CHANGE_TARGET} & git diff --name-status origin/${CHANGE_TARGET}...HEAD | cut -f2-",
-            returnStdout: true
-            ).trim().split('\n')
-        : sh(
-            script: "git diff --name-status HEAD^ HEAD | cut -f2-",
-            returnStdout: true
-            ).trim().split('\n')
+    def listFiles = sh(
+        script: "git diff --name-status HEAD^ HEAD | cut -f2-",
+        returnStdout: true
+        ).trim().split('\n')
     
     // def listedFiles = listFiles.split('\n')
     def ignoreList = env.IGNORE_FILES.split(',').collect { it.trim() }
