443c1b7b9438e285b98142fee4d9f019a8193ed5
Bruna Lima <bruna.lima9254@gmail.com>
2026-02-02 11:39:28 -0300

    update lib dockerfileCheck

--- SharedLibrary/vars/dockerfileCheck.groovy
+++ SharedLibrary/vars/dockerfileCheck.groovy
@@ -1,12 +1,16 @@
 #!/usr/bin/env groovy
 def call(Map stageParams) {
-    sh "docker rmi ${stageParams.imageName} || true"
-    
-    echo "Executando Dockerfile check sintaxe"
-    sh "docker build -f ${stageParams.dockerfilePath} --check ."
-    
-    echo "Executando Dockerfile check build - Com log detalhado"
-    sh "docker build -f ${stageParams.dockerfilePath} -t ${stageParams.imageName} --progress=plain ."
+    withCredentials([string(credentialsId: "${env.registryUrl}", variable: 'registryUrl')]) {
+        docker.withRegistry("https://${registryUrl}", env.registryCredential) {
+            sh "docker rmi ${stageParams.imageName} || true"
+            
+            echo "Executando Dockerfile check sintaxe"
+            sh "docker build -f ${stageParams.dockerfilePath} --check ."
+            
+            echo "Executando Dockerfile check build - Com log detalhado"
+            sh "docker build -f ${stageParams.dockerfilePath} -t ${stageParams.imageName} --progress=plain ."
 
-    sh "docker rmi ${stageParams.imageName}"
+            sh "docker rmi ${stageParams.imageName}"
+        }
+    }
 }
