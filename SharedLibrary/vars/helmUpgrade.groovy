1bbc60b9ed88c1b86f5fb092904fe4fa03f237c9
Bruna Lima <bruna.lima9254@gmail.com>
2026-02-03 16:22:46 -0300

    add lib helmUpgrade

--- SharedLibrary/vars/helmUpgrade.groovy
+++ SharedLibrary/vars/helmUpgrade.groovy
@@ -2,13 +2,10 @@
 
 def call(Map stageParams) {
 
-    def packageManager = stageParams.packageManager 
-    def testCommand = stageParams.testCommand
-    def nodeVersion = stageParams.nodeVersion
+    def valuesFile = stageParams.valuesFile
 
     withCredentials([
         file(credentialsId: "${env.kubeconfig}", variable: 'config'),
-        string(credentialsId: 'MYSQL_QA', variable: 'database')
         ]){
         
         sh '''
@@ -19,34 +16,16 @@ def call(Map stageParams) {
 
             cd "${helmDir}"
 
-            echo "CONFIGURANDO AMBIENTE"
-            export BRANCHNAME="${branchname}"
-            export AMBIENTE="${ambiente}"
-            export DATABASE_HOST="$database"
-            export DATABASE_NAME="${project}_${ambiente}"
-            export DATABASE_USER="usr_${project}_${ambiente}"
-            export IMAGE_TAG="${TAG}"
-            export INGRESS_HOST="${ambiente}-${project}.sme.prefeitura.sp.gov.br"
-            export WP_URL="http://${INGRESS_HOST}"
-
-            envsubst < values.yaml > values-${ambiente}.yaml
-
-            echo "REALIZANDO HELM UPGRADE"
             helm upgrade ${project} . \
                 --namespace ${ambiente}-${project} \
                 --set replicaCount=1 \
-                --values values-${ambiente}.yaml \
+                --values ${valuesFile} \
                 --install \
                 --history-max 3 \
                 --timeout 1m0s \
                 --atomic \
                 #--debug \
                 --wait
-        '''
-
-        sh '''
-            echo "REALIZANDO RESTART DO DEPLOYMENT"
-            kubectl rollout restart deployment/${project}-wp-stack -n ${ambiente}-${project}
         '''
     }
 }
