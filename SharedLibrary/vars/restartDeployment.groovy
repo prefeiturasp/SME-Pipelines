5b3764aaf2bf3bede9c3f9d1f131ad2b880007d3
Bruna Lima <bruna.lima9254@gmail.com>
2026-02-09 20:17:28 -0300

    lib restartDeployment

--- SharedLibrary/vars/restartDeployment.groovy
+++ SharedLibrary/vars/restartDeployment.groovy
@@ -2,15 +2,40 @@
 
 def call(Map stageParams) {
     
-    withCredentials([file(credentialsId: "${env.kubeconfig}", variable: 'config')]){
+    withCredentials([
+        file(credentialsId: "${env.kubeconfig}", variable: 'config'),
+        string(credentialsId: "${env.registryUrl}", variable: 'registryUrl')
+    ]){
+        config()
+
+        if (TAG?.trim()) {
+            setImage()
+        } else {
+            rollout()
+        }
+    }
+
+    def config = {
         sh """
             [ -f "\$HOME/.kube/config" ] && rm -f "\$HOME/.kube/config"
-
             mkdir -p "\$HOME/.kube"
             cp "\$config" "\$HOME/.kube/config"
+        """
+    }
+    
+    def rollout = {
+        sh """
             export KUBECONFIG="\$HOME/.kube/config"
-
             kubectl rollout restart deployment/${stageParams.deploymentName} -n ${stageParams.namespace}
+        """
+    }
+
+    def setImage = {
+        sh """
+            export KUBECONFIG="\$HOME/.kube/config"
+            kubectl set image deployment/${stageParams.deploymentName} \
+                ${stageParams.deploymentName}=${env.registryUrl}/${project}/${branchname}:${TAG} \
+                -n ${stageParams.namespace}
         """
     }
 }
