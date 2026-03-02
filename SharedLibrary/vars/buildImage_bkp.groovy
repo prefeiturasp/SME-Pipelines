c7410bcb3dcf11d73b9e92a1f2e3289de88534d9
Bruna Lima <bruna.lima9254@gmail.com>
2026-03-02 12:14:16 -0300

    migração jenkinsfile-acervo para shared libs

---
+++ SharedLibrary/vars/buildImage_bkp.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+def call(Map stageParams) {
+
+    def fullImageName = ""
+    withCredentials([string(credentialsId: "${env.registryUrl}", variable: 'registryUrl')]) {
+        fullImageName = "${registryUrl}/${env.BRANCH_NAME}/${stageParams.imageName}"
+        def dockerImage = docker.build(fullImageName, "-f ${stageParams.dockerfilePath} .")
+
+        if (stageParams.sendRegistry == "yes") {
+            docker.withRegistry("https://${registryUrl}", env.registryCredential) {
+                dockerImage.push()
+            }
+        }
+    }
+
+    sh "docker rmi ${fullImageName}"
+}
