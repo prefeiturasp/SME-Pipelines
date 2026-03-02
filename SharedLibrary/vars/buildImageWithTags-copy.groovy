808ff5eecfa7d5b124fb8803a961143357f023e0
Bruna Lima <bruna.lima9254@gmail.com>
2026-03-02 09:50:28 -0300

    lib buildImageWithTags-copy

---
+++ SharedLibrary/vars/buildImageWithTags-copy.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/env groovy
+
+def call(Map stageParams) {
+
+    def imageName = stageParams.imageName
+    
+    def fullImageName = ""
+    def commitHash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+    env.TAG = "${commitHash}"
+
+    withCredentials([string(credentialsId: "${env.registryUrl}", variable: 'registryUrl')]) {
+        docker.withRegistry("https://${registryUrl}", env.registryCredential) {
+            
+            if (imageName?.trim()) {
+                fullImageName = "${registryUrl}/${env.project}/${env.branchname}/${stageParams.imageName}"
+            } else {
+                fullImageName = "${registryUrl}/${env.project}/${env.branchname}"
+            }
+            
+            sh """
+                docker build \
+                    --cache-from ${fullImageName} \
+                    -t ${fullImageName} \
+                    -f ${stageParams.dockerfilePath} .
+            """
+
+            sh "docker tag ${fullImageName} ${fullImageName}:${TAG}"
+
+            if (stageParams.sendRegistry == "yes") {
+                sh "docker push ${fullImageName}:${TAG}"
+                sh "docker push ${fullImageName}"
+            }
+        }
+    }
+
+    sh "docker rmi ${fullImageName}"
+    sh "docker rmi ${fullImageName}:${TAG}"
+}
