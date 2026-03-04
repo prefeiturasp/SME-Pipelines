3a10f7f1e07bd39c5f9ea470a7244c762db55da0
Bruna Lima <bruna.lima9254@gmail.com>
2026-03-04 11:45:31 -0300

    lib sonarqubeReact | remove retry

--- SharedLibrary/vars/sonarqubeReact.groovy
+++ SharedLibrary/vars/sonarqubeReact.groovy
@@ -18,39 +18,35 @@ def call(Map stageParams) {
             }
             
             if (!env.BRANCH_NAME.startsWith('PR-')) {
-                retry(1) {
-                    sh"""
-                        ${scannerHome}/bin/sonar-scanner \
-                            -Dsonar.projectKey=${SONAR_PROJECT} \
-                            -Dsonar.branch.name=${branchname} \
-                            -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
-                            -Dsonar.exclusions="${sonarExclusions}" \
-                            -Dsonar.coverage.exclusions="${coverageExclusions}"  \
-                            -Dsonar.docker.file.patterns=${dockerfilePath} \
-                            -Dsonar.scm.provider=git \
-                            -Dsonar.sources=.
-                    """
-                }
+                sh"""
+                    ${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=${SONAR_PROJECT} \
+                        -Dsonar.branch.name=${branchname} \
+                        -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
+                        -Dsonar.exclusions="${sonarExclusions}" \
+                        -Dsonar.coverage.exclusions="${coverageExclusions}"  \
+                        -Dsonar.docker.file.patterns=${dockerfilePath} \
+                        -Dsonar.scm.provider=git \
+                        -Dsonar.sources=.
+                """
             } else {
-                retry(1) {
-                    sh "git fetch origin refs/heads/${env.CHANGE_TARGET}:refs/remotes/origin/${env.CHANGE_TARGET}"
-                    sh "git show-ref | grep ${env.CHANGE_TARGET}"
+                sh "git fetch origin refs/heads/${env.CHANGE_TARGET}:refs/remotes/origin/${env.CHANGE_TARGET}"
+                sh "git show-ref | grep ${env.CHANGE_TARGET}"
 
-                    sh"""
-                        ${scannerHome}/bin/sonar-scanner \
-                            -Dsonar.projectKey=${SONAR_PROJECT} \
-                            -Dsonar.pullrequest.branch=${env.CHANGE_BRANCH} \
-                            -Dsonar.pullrequest.base=${env.CHANGE_TARGET} \
-                            -Dsonar.pullrequest.key=${env.CHANGE_ID} \
-                            -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
-                            -Dsonar.typescript.tsconfigPath=tsconfig.json \
-                            -Dsonar.exclusions="${sonarExclusions}" \
-                            -Dsonar.coverage.exclusions="${coverageExclusions}"  \
-                            -Dsonar.docker.file.patterns=${dockerfilePath} \
-                            -Dsonar.scm.provider=git \
-                            -Dsonar.sources=.
-                    """
-                }
+                sh"""
+                    ${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=${SONAR_PROJECT} \
+                        -Dsonar.pullrequest.branch=${env.CHANGE_BRANCH} \
+                        -Dsonar.pullrequest.base=${env.CHANGE_TARGET} \
+                        -Dsonar.pullrequest.key=${env.CHANGE_ID} \
+                        -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
+                        -Dsonar.typescript.tsconfigPath=tsconfig.json \
+                        -Dsonar.exclusions="${sonarExclusions}" \
+                        -Dsonar.coverage.exclusions="${coverageExclusions}"  \
+                        -Dsonar.docker.file.patterns=${dockerfilePath} \
+                        -Dsonar.scm.provider=git \
+                        -Dsonar.sources=.
+                """
             }
         }
     }
