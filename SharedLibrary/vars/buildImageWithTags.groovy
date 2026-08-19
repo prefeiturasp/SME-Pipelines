f3bc991a0057500dc366c11085d4b5270fedfd72
Bruna Lima <bruna.lima9254@gmail.com>
2026-08-19 10:24:46 -0300

    Update lib buildImageWithTags

--- SharedLibrary/vars/buildImageWithTags.groovy
+++ SharedLibrary/vars/buildImageWithTags.groovy
@@ -9,7 +9,7 @@ def call(Map stageParams) {
     env.SOURCE_BRANCH = getSourceBranch() ?: ''
     
     env.TAG1 = "${commitHash}"
-    env.TAG2 = env.SOURCE_BRANCH?.toLowerCase()?.replaceAll("\\s", "")?.replace("/", "-")
+    env.TAG2 = env.SOURCE_BRANCH?.toLowerCase()?.replaceAll("\\s", "-")?.replace("/", "-")
     
     echo "TAG1: ${env.TAG1}"
     echo "TAG2: ${env.TAG2}"
