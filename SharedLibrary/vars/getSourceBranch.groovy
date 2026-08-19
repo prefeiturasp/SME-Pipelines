a1ac12a0335d1b133b4e32c660b9dc445ee3a314
Bruna Lima <bruna.lima9254@gmail.com>
2026-08-19 10:28:56 -0300

    Update lib buildImageWithTags

--- SharedLibrary/vars/getSourceBranch.groovy
+++ SharedLibrary/vars/getSourceBranch.groovy
@@ -4,10 +4,16 @@ def call() {
         returnStdout: true
     ).trim()
 
+    // Match GitHub pull requests
     def matcher = msg =~ /from\s+[^\/]+\/(.+)/
-
     if (matcher) {
         return matcher[0][1].trim()
+    }
+
+    // Match GitLab merge requests
+    def gitlabMatcher = msg =~ /Merge branch '([^']+)'/
+    if (gitlabMatcher) {
+        return gitlabMatcher[0][1].trim()
     }
 
     return null
