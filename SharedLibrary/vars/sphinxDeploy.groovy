cd95f4bc50ce10199d524cdc30694dece8c0d6a1
Bruna Lima <bruna.lima9254@gmail.com>
2026-09-21 22:48:33 -0300

    atualiza libs

--- SharedLibrary/vars/sphinxDeploy.groovy
+++ SharedLibrary/vars/sphinxDeploy.groovy
@@ -6,7 +6,6 @@ def call(Map stageParams) {
     def credentialsId = stageParams.credentialsId
 
     sh 'python -m pip install --root-user-action=ignore ".[docs]"'
-    sh 'pip install sphinx furo'
 
     try {
         sh 'make -C docs html'
