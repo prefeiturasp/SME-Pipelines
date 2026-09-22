b41f5620a717bee34b95760cf68f2ed5e2dab025
Bruna Lima <bruna.lima9254@gmail.com>
2026-09-21 22:40:26 -0300

    atualiza libs

--- SharedLibrary/vars/sphinxDeploy.groovy
+++ SharedLibrary/vars/sphinxDeploy.groovy
@@ -6,8 +6,13 @@ def call(Map stageParams) {
     def credentialsId = stageParams.credentialsId
 
     sh 'python -m pip install --root-user-action=ignore ".[docs]"'
+    sh 'pip install sphinx'
 
-    sh 'make -C docs html'
+     try {
+        sh 'make -C docs html'
+    } catch (err) {
+        sh 'sphinx-build -b html docs/ docs/_build/html'
+    }
 
     sh 'pip install --root-user-action=ignore ghp-import'
 
